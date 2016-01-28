e75dcc3573a95988be23bb45de25eb3af0cfeb3b
Tô Thành Công <tcvn1985@gmail.com>
2016-01-28 12:03:03 +0700

    String format

--- baitap/test.py
+++ baitap/test.py
@@ -1,7 +1,9 @@
-name = 'HOC CHU DONG'
+tuoi = 2
+ho_va_ten = "HOC CHU DONG"
 
-print ('Xin chao ' + name)
+print '{0} co tuoi la {1}' .format(ho_va_ten, tuoi)
+print 'Toi ten la: {0}' .format(ho_va_ten)
 
-print ('Xin chao %s' %name)
 
-print ('Xin chao {}' .format(name))
+print  ho_va_ten, 'co tuoi la ' , tuoi
+print 'Toi ten la: ', ho_va_ten
