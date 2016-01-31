dbfa345b0ce8ffeef04216fc9a8edc0ea95a378a
Tô Thành Công <tcvn1985@gmail.com>
2016-01-31 10:58:51 +0700

    update về biến

--- baitap/test.py
+++ baitap/test.py
@@ -1,9 +1,13 @@
 tuoi = 2
 ho_va_ten = "HOC CHU DONG"
 
+#Nen su dung kieu print nhu sau
 print '{0} co tuoi la {1}' .format(ho_va_ten, tuoi)
 print 'Toi ten la: {0}' .format(ho_va_ten)
 
-
+# Tranh su dung kieu print duoi day
 print  ho_va_ten, 'co tuoi la ' , tuoi
 print 'Toi ten la: ', ho_va_ten
+
+#
+print '{0:.4f}' .format(1.0/3)
