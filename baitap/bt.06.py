495d73887a88d2a8064068626476557a4aa1303f
Tô Thành Công <tcvn1985@gmail.com>
2016-02-03 18:15:44 +0700

    Toán tử và biểu thức
    Toán tử và biểu thức

---
+++ baitap/bt.06.py
@@ -0,0 +1,9 @@
+#!/bin/python
+
+print 4 + 5     #Phep cong
+print 5 - 9     #Phep tru
+print 5 * 6     #Phep nhan
+print 15 / 8    #Phep chia, luu y: ket qua la phan nguyen
+print 15.0 / 8  #Ep kieu de lay dung gia tri cua phep chia
+print 15 % 8    #Phep chia lay du
+print -25.5 % 2.2 
