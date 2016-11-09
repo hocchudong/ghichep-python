bdb169223d3617ca6021ebcb691624cdd510568c
datkk06 <thanhdatkma96@gmail.com>
2016-11-09 15:36:48 +0700

    cap nhat va thay doi

---
+++ P4E-VN/code/Chapter1+2/GT-ban-than.py
@@ -0,0 +1,9 @@
+#Viet mot chuong trinh gioi thieu ve ban than
+
+print "Toi ten la Pham Thanh Dat"
+print "Nam nay toi 20 tuoi"
+print "Sinh vien nam 3 Hoc Vien Ky Thuat mat Ma"
+print "Dang la thuc tap sinh tai VNPT"
+print "Cong viec chinh cua toi la Mang va He Thong"
+print "So thich Bong Da va Am Nhac"
+print "Cam on cac ban !!!"
---
+++ P4E-VN/ly-thuyet/new-word.md
@@ -0,0 +1,10 @@
+#Từ mới.
+
+```sh
+obvious : rõ ràng
+unpredictable : Không thể đoán trước.
+yield : Năng suất.
+nondeterministic : Toán không đơn định.
+pseudorandom : Giả ngẫu nhiên
+parentheses : Ngoặc
+```
