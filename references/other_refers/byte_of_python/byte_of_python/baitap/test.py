d3a16ef2092377dd9d4643ef0764a6e5ace6784a
Nguyen Hoai Nam <namptit307@gmail.com>
2017-10-08 23:28:27 +0700

    Update references

---
+++ references/other_refers/byte_of_python/byte_of_python/baitap/test.py
@@ -0,0 +1,28 @@
+tuoi = 2
+ho_va_ten = "HOC CHU DONG"
+
+#Nen su dung kieu print nhu sau
+print '{0} co tuoi la {1}' .format(ho_va_ten, tuoi)
+print 'Toi ten la: {0}' .format(ho_va_ten)
+
+# Tranh su dung kieu print duoi day
+print  ho_va_ten, 'co tuoi la ' , tuoi
+print 'Toi ten la: ', ho_va_ten
+
+#
+print '{0:.4f}' .format(1.0/3)
+
+#
+print "HOC"
+print "CHU"
+print "DONG"
+
+#
+print "HOC",
+print "CHU",
+print "DONG"
+
+print '{:.3f}' .format(1.0/3)
+print '{:a^11}' .format('hello')
+print '{:a<10}'.format('test')
+print 'Is\'t  \t me'
